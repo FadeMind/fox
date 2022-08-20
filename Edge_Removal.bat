66019708b1c6091e7a46bc7644e7c305c94d1914
AveYo <aveyo@yahoo.com>
2022-08-20 11:03:51 +0300

    2022.08.20 like a fine wine
    cmd window flashing - no more ;)

--- Edge_Removal.bat
+++ Edge_Removal.bat
@@ -1,7 +1,7 @@
 @(set "0=%~f0"^)#) & powershell -nop -c iex([io.file]::ReadAllText($env:0)) & exit/b
 #:: double-click to run or just copy-paste into powershell - it's a standalone hybrid script
 #::
-$_Paste_in_Powershell = { $host.ui.RawUI.WindowTitle = 'Edge Removal - AveYo, 2022.08.19'
+$_Paste_in_Powershell = { $host.ui.RawUI.WindowTitle = 'Edge Removal - AveYo, 2022.08.20'
 
 $also_remove_webview = 1
 
@@ -69,47 +69,32 @@ foreach ($i in $remove_appx) {
 ##################################################################################################################################
 $ChrEdgeFkOff = @'
 @echo off
-::# toggle when launched without arguments, else jump to arguments: "install" or "remove"
+
 set CLI=%*&(set IFEO=HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options&set MSE=&set BHO=&set ProgID=)
 call :reg_var "HKCR\MSEdgeMHT\shell\open\command" "" ProgID
 for %%. in (%ProgID%) do if not defined MSE set "MSE=%%~."& set "MSEPath=%%~dp."
-rem if /i "%CLI%"=="" reg query "%IFEO%\ie_to_edge_stub.exe\0" /v Debugger >nul 2>nul && goto remove || goto install
-rem if /i "%~1"=="install" (goto install) else if /i "%~1"=="remove" goto remove
 
 :install
 if defined MSEPath for /f "delims=" %%W in ('dir /o:D /b /s "%MSEPath%\*ie_to_edge_stub.exe"') do set "BHO=%%~fW"
 if not exist "%MSEPath%chredge.exe" if exist "%MSE%" mklink /h "%MSEPath%chredge.exe" "%MSE%" >nul
 if defined BHO copy /y "%BHO%" "%ProgramData%\" >nul 2>nul
 call :export ChrEdgeFkOff_cmd > "%ProgramData%\ChrEdgeFkOff.cmd"
-reg add HKCR\microsoft-edge /f /ve /d URL:microsoft-edge >nul
-reg add HKCR\microsoft-edge /f /v "URL Protocol" /d "" >nul
-reg add HKCR\microsoft-edge /f /v "NoOpenWith" /d "" >nul
-reg add HKCR\microsoft-edge\shell\open\command /f /ve /d "\\"%ProgramData%\ie_to_edge_stub.exe\\" %%1" >nul
-reg add HKCR\MSEdgeHTM /f /v "NoOpenWith" /d "" >nul
-reg add HKCR\MSEdgeHTM\shell\open\command /f /ve /d "\\"%ProgramData%\ie_to_edge_stub.exe\\" %%1" >nul
+set "Headless_Console_by_AveYo=%systemroot%\system32\conhost.exe --headless" & rem still innovating
+reg add "HKCR\microsoft-edge" /f /ve /d URL:microsoft-edge >nul
+reg add "HKCR\microsoft-edge" /f /v "URL Protocol" /d "" >nul
+reg add "HKCR\microsoft-edge" /f /v "NoOpenWith" /d "" >nul
+reg add "HKCR\microsoft-edge\shell\open\command" /f /ve /d "\\"%ProgramData%\ie_to_edge_stub.exe\\" %%1" >nul
+reg add "HKCR\MSEdgeHTM" /f /v "NoOpenWith" /d "" >nul
+reg add "HKCR\MSEdgeHTM\shell\open\command" /f /ve /d "\\"%ProgramData%\ie_to_edge_stub.exe\\" %%1" >nul
 reg add "%IFEO%\ie_to_edge_stub.exe" /f /v UseFilter /d 1 /t reg_dword >nul >nul
 reg add "%IFEO%\ie_to_edge_stub.exe\0" /f /v FilterFullPath /d "%ProgramData%\ie_to_edge_stub.exe" >nul
-reg add "%IFEO%\ie_to_edge_stub.exe\0" /f /v Debugger /d "\\"%ProgramData%\ChrEdgeFkOff.cmd\\"" >nul
+reg add "%IFEO%\ie_to_edge_stub.exe\0" /f /v Debugger /d "%Headless_Console_by_AveYo% \\"%ProgramData%\ChrEdgeFkOff.cmd\\"" >nul
 reg add "%IFEO%\msedge.exe" /f /v UseFilter /d 1 /t reg_dword >nul
 reg add "%IFEO%\msedge.exe\0" /f /v FilterFullPath /d "%MSE%" >nul
-reg add "%IFEO%\msedge.exe\0" /f /v Debugger /d "\\"%ProgramData%\ChrEdgeFkOff.cmd\\"" >nul
+reg add "%IFEO%\msedge.exe\0" /f /v Debugger /d "%Headless_Console_by_AveYo% \\"%ProgramData%\ChrEdgeFkOff.cmd\\"" >nul
 if "%CLI%" neq "" exit /b
-rem echo;& %<%:f0 " ChrEdgeFkOff V6 "%>>% & %<%:2f " INSTALLED "%>>% & %<%:f0 " run again to remove "%>%
+rem echo;& %<%:f0 " ChrEdgeFkOff V7 "%>>% & %<%:2f " INSTALLED "%>>% & %<%:f0 " run again to remove "%>%
 rem timeout /t 7
-exit /b
-
-:remove
-del /f /q "%ProgramData%\ChrEdgeFkOff.*" "%MSEPath%chredge.exe" >nul 2>nul
-rem del /f /q "%ProgramData%\ie_to_edge_stub.exe"
-reg delete HKCR\microsoft-edge /f /v "NoOpenWith" >nul 2>nul
-reg add HKCR\microsoft-edge\shell\open\command /f /ve /d "\\"%MSE%\\" --single-argument %%1" >nul
-reg delete HKCR\MSEdgeHTM /f /v "NoOpenWith" >nul 2>nul
-reg add HKCR\MSEdgeHTM\shell\open\command /f /ve /d "\\"%MSE%\\" --single-argument %%1" >nul
-reg delete "%IFEO%\ie_to_edge_stub.exe" /f >nul 2>nul
-reg delete "%IFEO%\msedge.exe" /f >nul 2>nul
-if "%CLI%" neq "" exit /b
-echo;& %<%:f0 " ChrEdgeFkOff V6 "%>>% & %<%:df " REMOVED "%>>% & %<%:f0 " run again to install "%>%
-timeout /t 7
 exit /b
 
 :export: [USAGE] call :export NAME
@@ -118,8 +103,8 @@ set [=&for /f "delims=:" %%s in ('findstr /nbrc:":%~1:\[" /c:":%~1:\]" "%~f0"')d
 <"%~f0" ((for /l %%i in (0 1 %[%) do set /p =)&for /l %%i in (%[% 1 %]%) do (set txt=&set /p txt=&echo(!txt!)) &endlocal &exit /b
 
 :ChrEdgeFkOff_cmd:[
-@title ChrEdgeFkOff V6 & echo off & set ?= open start menu web search, widgets links or help in your chosen browser - by AveYo
-rem PoS Defender started screaming about the former vbs version, so now this window will flash briefly. tx microsoft "engineers"
+@title ChrEdgeFkOff V7 & echo off & set ?= open start menu web search, widgets links or help in your chosen browser - by AveYo
+rem PoS Defender started screaming about the former vbs version, so now this window will flash briefly. V7: not anymore ;)
 call :reg_var "HKCU\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice" ProgID ProgID
 if /i "%ProgID%" equ "MSEdgeHTM" echo;Default browser is set to Edge! Change it or remove ChrEdgeFkOff script. & pause & exit /b
 call :reg_var "HKCR\%ProgID%\shell\open\command" "" Browser
